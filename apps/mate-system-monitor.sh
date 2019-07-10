9bed06dce70033b5fc8235df43f859350348ae3c
Dario Pagani <you@example.com>
2019-07-10 22:15:58 +0200

    mate-system-monitor

--- apps/mate-system-monitor.sh
+++ apps/mate-system-monitor.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+apt-get -y install mate-system-monitor
 
+# Show menu entry in Unity
+sed -i '/OnlyShowIn=MATE;/c\OnlyShowIn=MATE;Unity;' /tmp/foo
+
+if $REMOVE_GNOME
+then 
+	apt-get -y remove gnome-system-monitor
+fi
