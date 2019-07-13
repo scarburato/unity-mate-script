6c3a2bd2df2c683989d1c4f8e8f3a7e0f7b77d4b
Dario Pagani <you@example.com>
2019-07-13 14:06:55 +0200

    Font fix

---
+++ apps/interface.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Set mate to use Ubuntu fonts
+cp files/unity-mate-interface.gschema.override /usr/share/glib-2.0/schemas/ 
+chown root:root /usr/share/glib-2.0/schemas/unity-mate-interface.gschema.override
